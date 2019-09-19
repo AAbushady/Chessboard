c9887c810d4d63e309f2f10a4f8fb3b654ec43f8
Alex Abushady <aabushady214@gmail.com>
2019-09-19 09:55:10 -0400

    Update chessboard.js
    fixed typos.

--- chessboard.js
+++ chessboard.js
@@ -1,6 +1,6 @@
 /*
-Write a programt hat creates a string that represents an 8x8 grid, using new-line
-charactesr to seperate lines. At each position of the grid there is either a space or
+Write a program that creates a string that represents an 8x8 grid, using new-line
+characters to seperate lines. At each position of the grid there is either a space or
 a # character. The characters should form a chessboard. Passing the string to 
 console.log should show this:
 
