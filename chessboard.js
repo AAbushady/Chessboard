21abf5fb85b1290ea978f66e6e04e15953722f4c
Alex Abushady <aabushady214@gmail.com>
2019-09-19 11:12:40 -0400

    Update chessboard.js
    constant set for the row and column of board. Added comments.

--- chessboard.js
+++ chessboard.js
@@ -17,11 +17,19 @@ After making this pattern, define a binding size = 8 and change the program so t
 for any size, out-putting a grid of the given width and height.
 */
 
+// Constants and Variables.
+// Set row to set the grid up.
+const row = 8;
+// col will = row so grid is always NxN.
+const col = row;
+// Sets up the chessboard.
 var chessboard = "";
 
-for (var cnt1 = 0; cnt1 < 8; cnt1++) {
-    for (var cnt2 = 0; cnt2 < 8; cnt2++) {
-        if ((cnt1 + cnt2) % 2 ==0) {
+// This iteration of the for loop handles the rows and the new-lines.
+for (var cnt1 = 0; cnt1 < row; cnt1++) {
+    // This iteration handles the columns and decides whether it is a # or a " ".
+    for (var cnt2 = 0; cnt2 < col; cnt2++) {
+        if ((cnt1 + cnt2) % 2 == 0) {
             chessboard += " ";
         } else {
             chessboard += "#";
@@ -30,4 +38,5 @@ for (var cnt1 = 0; cnt1 < 8; cnt1++) {
     chessboard += "\n";
 }
 
+// Output the chessboard.
 console.log(chessboard);
