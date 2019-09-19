87f427489b9100e2c7073c15562e7aa51f6777c3
Alex Abushady <aabushady214@gmail.com>
2019-09-19 11:06:56 -0400

    Update chessboard.js
    Hard coded version complete

--- chessboard.js
+++ chessboard.js
@@ -16,3 +16,18 @@ console.log should show this:
 After making this pattern, define a binding size = 8 and change the program so that it works
 for any size, out-putting a grid of the given width and height.
 */
+
+var chessboard = "";
+
+for (var cnt1 = 0; cnt1 < 8; cnt1++) {
+    for (var cnt2 = 0; cnt2 < 8; cnt2++) {
+        if ((cnt1 + cnt2) % 2 ==0) {
+            chessboard += " ";
+        } else {
+            chessboard += "#";
+        }
+    }
+    chessboard += "\n";
+}
+
+console.log(chessboard);
