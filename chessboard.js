b7950f1f943eeebcf92f0e13141849f247a2ec39
Alex Abushady <aabushady214@gmail.com>
2019-09-19 09:52:49 -0400

    Create chessboard.js
    Added Problem Statement.

---
+++ chessboard.js
@@ -0,0 +1,18 @@
+/*
+Write a programt hat creates a string that represents an 8x8 grid, using new-line
+charactesr to seperate lines. At each position of the grid there is either a space or
+a # character. The characters should form a chessboard. Passing the string to 
+console.log should show this:
+
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+
+After making this pattern, define a binding size = 8 and change the program so that it works
+for any size, out-putting a grid of the given width and height.
+*/
