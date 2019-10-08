17bf57ed9ed28fa509177f40218688d06e3246e2
Alex Abushady <aabushady214@gmail.com>
2019-10-08 11:58:28 -0400

    Update chessboard.js

--- chessboard.js
+++ chessboard.js
@@ -1,3 +1,6 @@
+/*global console*/
+/*jshint esversion:6*/
+
 // Write a program that creates a string that represents an 8x8 grid, using new-line
 // characters to seperate lines. At each position of the grid there is either a space or
 // a # character. The characters should form a chessboard. Passing the string to 
@@ -15,7 +18,7 @@
 // After making this pattern, define a binding size = 8 and change the program so that it works
 // for any size, out-putting a grid of the given width and height.
 (function () {
-    "using strict";
+    "use strict";
 
     // Constants and Variables.
     // Set row to set the grid up.
